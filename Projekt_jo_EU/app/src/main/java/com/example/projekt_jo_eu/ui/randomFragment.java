b4f16774f8a750028a074973c9f55161b2e95c49
saribence <saribence10@gmail.com>
2025-11-17 20:24:41 +0100

    Megcsináltam a random országnak valo fragmentet.

---
+++ Projekt_jo_EU/app/src/main/java/com/example/projekt_jo_eu/ui/randomFragment.java
@@ -0,0 +1,26 @@
+package com.example.projekt_jo_eu.ui;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.projekt_jo_eu.R;
+
+public class randomFragment extends Fragment {
+
+    public randomFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.random_layout, container, false);
+
+
+        return view;
+    }
+}
